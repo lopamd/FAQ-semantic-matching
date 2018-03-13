705acc1ac37116d219d6c62b8d22135a824fd928
Stephen Balhoff <balhoff116@gmail.com>
2018-03-12 23:34:29 -0500

    Added properties to the feature extraction class.

--- base_objects.py
+++ base_objects.py
@@ -7,6 +7,9 @@ class QAFeatureExtraction( object ):
     '''qa_pairs is a list of QAPair objects'''
     def __init__( self, qa_pairs ):
         self.qa_pairs = qa_pairs
+        self._tokens = None
+        self._lemmas = None
+        self._stems = None
 
     '''Private abstract function to tokenize the questions and answers'''
     def __tokenize( self ):
@@ -19,3 +22,21 @@ class QAFeatureExtraction( object ):
     '''Private abstract function to stem the questions and answers'''
     def __stem( self ):
         raise NotImplementedError("Class %s doesn't implement stem()" % (self.__class__.__name__))
+        
+    @property
+    def tokens(self):
+        if self._tokens is None:
+            self.__tokenize()
+        return self._tokens
+    
+    @property
+    def lemmas(self):
+        if self._lemmas is None:
+            self.__lemmatize()
+        return self._lemmas
+        
+    @property
+    def stems(self):
+        if self._stems is None:
+            self.__stem()
+        return self._stems
