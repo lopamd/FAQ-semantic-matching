565c8bbd2cf6ef604d654b30543b2062c9f0f492
Stephen Balhoff <balhoff116@gmail.com>
2018-03-12 23:06:52 -0500

    Initial commit. Only has a way to read in the faqs.

---
+++ faq_config.py
@@ -0,0 +1,36 @@
+import csv
+
+inputfile = 'input/Java_FAQ.csv'
+
+class QAPair:
+     def __init__(self, question, answer):
+         self.question = question
+         self.answer = answer
+
+class FAQReader( object ):
+    """Abstract class. Please implement fetch to return a list of QAPairs."""
+    def fetch( self ):
+        raise NotImplementedError("Class %s doesn't implement fetch()" % (self.__class__.__name__))
+        
+class CSVFAQReader(FAQReader):
+    def __init__(self, csvfilename):
+        self.csvfilename = csvfilename
+
+    def fetch( self ):
+        faqs = []
+        with open(self.csvfilename) as csvfile:
+            areader = csv.reader(csvfile)
+            for row in areader:
+                faqs.append(QAPair(row[0].strip(), row[1].strip()))
+        return faqs
+        
+        
+def getFAQs():
+    faqreader = CSVFAQReader(inputfile)
+    return faqreader.fetch()
+  
+#Usage:
+#import faq_config
+#for y in faq_config.getFAQs():
+#    print(y.question)
+#    print(y.answer)
