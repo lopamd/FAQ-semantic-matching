04ed9f7a097a7e7589468d89ce04462b35fe8834
Lopamudra Muduli <lopa147@gmail.com>
2018-04-02 00:27:36 -0700

    Support posix enviornment (Mac or Unix) as well.
    Signed-off-by: Lopamudra Muduli <lopa147@gmail.com>

--- nlp_config.py
+++ nlp_config.py
@@ -1,7 +1,14 @@
+import os
+
 default_locale = 'english'
 
 faq_input_file = 'input/Java_FAQ.csv'
 
-path_to_stanford_lib = r'deps\stanford-corenlp-full-2018-02-27'
-path_to_stanford_jar = path_to_stanford_lib + r'\stanford-corenlp-3.9.1.jar'
-path_to_stanford_models_jar = path_to_stanford_lib + r'\stanford-corenlp-3.9.1-models.jar'
+if (os.name == "posix"):
+	path_to_stanford_lib = r'deps/stanford-corenlp-full-2018-02-27'
+	path_to_stanford_jar = path_to_stanford_lib + r'/stanford-corenlp-3.9.1.jar'
+	path_to_stanford_models_jar = path_to_stanford_lib + r'/stanford-corenlp-3.9.1-models.jar'
+else:
+	path_to_stanford_lib = r'deps\stanford-corenlp-full-2018-02-27'
+	path_to_stanford_jar = path_to_stanford_lib + r'\stanford-corenlp-3.9.1.jar'
+	path_to_stanford_models_jar = path_to_stanford_lib + r'\stanford-corenlp-3.9.1-models.jar'
