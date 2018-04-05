8c263da476bd68e0d773052a47ab934f80195a96
Lopamudra Muduli <lopa147@gmail.com>
2018-04-04 23:26:15 -0700

    Base class for all algorithms
    Signed-off-by: Lopamudra Muduli <lopa147@gmail.com>

---
+++ nlp_algo.py
@@ -0,0 +1,30 @@
+class NLPAlgorithm:
+    def __init(self, uquestion):
+	
+	self.user_question = uquestion
+        #This will contain the evaluation score
+        self.score = None
+
+        #Final results
+        self.results = None
+    '''Private abstract function to implement the actual algorithm'''
+    def _compute(self):
+        raise NotImplementedError("Class %s doesn't implement _compute()" % (self.__class__.__name__))
+
+    '''Private abstract function to evaluate the output'''
+    def _evaluate(self):
+        raise NotImplementedError("Class %s doesn't implement _evaluate()" % (self.__class__.__name__))
+
+    '''Function to print the output'''
+    def _print(self):
+        print("Final Score is")
+
+class BOWAlgorithm(NLPAlgorithm):
+    def __init__(self, uquestion):
+        super().__init__(uquestion)
+
+    def _compute():
+        pass
+    
+    def _evaluate():
+        pass
