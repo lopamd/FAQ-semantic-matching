380f75a7121fae0f4cd9b64a3f2c0f65f8dee55a
Lopamudra Muduli <lopa147@gmail.com>
2018-04-08 16:33:09 -0500

    BOW algorithm implementation complete.
    Here is a example run:
    
    ****** Hummingbird FAQ engine powered by NLTK *********
    Input your question:How fast do hummingbirds' wings beat per second?
    ***********************************************************************
    Given user question:  How fast do hummingbirds' wings beat per second?
    ***********************************************************************
    Top 10 results from Bag of words algorithm are:
    Hummingbirds' wings can beat at about 70 times per second in normal fight and about 200 times per second during a high speed dive. 0.914659120760047
    It depends on the size of the bird. The largest, the Giant Hummingbird, has a wingbeat rate of 10-15 per second. The fastest recorded rate was about 80 per second, on a tiny Amethyst Woodstar, and the slightly smaller Bee Hummingbird - the world's smallest bird - may have an even faster rate. Common small North American hummingbirds like the Ruby-throated and Rufous average around 53 per second in normal flight. 0.8520128672302583
    A rate of 1260 beats per minute was measured in a Blue-throated Hummingbird. In torpid hummingbirds, the heart rate can drop to 50-180 per minute. 0.7777777777777777
    A hummingbird can lick 10-15 times per second while feeding. 0.7453559924999298
    Hummingbirds can fly in the rain and, like dogs, shake their heads to dispel drops of water. Unlike dogs, however, a hummingbird shakes its head violently, 132 times per second, and rotating 202 degrees - all while flying and maintaining direction! 0.6666666666666666
    A hummingbird flies at an average speed of 25-30 miles per hour and can do a fast dive at up to 60 miles per hour. 0.6324555320336759
    There are more than 325 unique hummingbird species in the world. Only eight species regularly breed in the United States, though up to two dozen species may visit the country or be reported as regular vagrants. The rest of the hummingbirds are primarily tropical species and do not regularly migrate. They are found in Central and South America as well as throughout the Caribbean. 0.5773502691896257
    Hummingbirds have 1,000-1,500 feathers, the fewest number of feathers of any bird species in the world. Not only do they not need as many feathers because of their tiny size, but fewer feathers also keeps them more lightweight for easier flight. 0.5773502691896257
    At rest, a hummingbird takes an average of 250 breaths per minute. Their breathing pace will increase when they are in flight. 0.5443310539518174
    Female hummingbirds lay only two eggs. 0.5443310539518174
    
    Signed-off-by: Lopamudra Muduli <lopa147@gmail.com>

--- faq_nlp.py
+++ faq_nlp.py
@@ -1,29 +1,53 @@
 import nltk_objects
 import faq_config
 import base_objects
+import operator
+
+from nlp_algo import BOWAlgorithm
+
+RESULTS_TOPN = 10
 
 def main():
 
     print("****** Hummingbird FAQ engine powered by NLTK *********")
+ 
     faqs = faq_config.getFAQs()
     feature_extractor = nltk_objects.NLTKFeatureExtraction(faqs)
     qatokens = feature_extractor.tokens
     qabows = feature_extractor.bow
 
-    '''
-    for bow in qabows:
-        print(bow) 
-    '''
-
     user_q = input("Input your question:")
-    
+    #user_q = "when is hummingbird season"
+    #user_q = "Where do hummingbirds go in the winter?"
+    #user_q = "How fast do hummingbirds' wings beat per second?"
+
+    if user_q == "" or user_q == None:
+        raise ValueError("Invalid question given. Exiting")
+        exit(1)
+ 
     #FIXME: It has to be added to the empty list because nltk_object operates on the list
     #Alt: Alternate approach. Only call __tokenize(). But move stops to a class variable.
 
     user_qa = [base_objects.QAPair(user_q, "")]
     user_feat_extractor = nltk_objects.NLTKFeatureExtraction(user_qa)
-    print(user_feat_extractor.tokens)
-    print(user_feat_extractor.bow)
+    #print(user_feat_extractor.tokens)
+    #print(user_feat_extractor.bow)
+
+    bow_algo = BOWAlgorithm(user_q, user_feat_extractor, feature_extractor)
+    resultDict = bow_algo._compute()
+    sortedResults = sorted(resultDict.items(), key=lambda x:x[1], reverse=True)
+    count = 0
+
+    print("***********************************************************************")
+    print("Given user question: ", user_q)
+    print("***********************************************************************")
+    print("Top 10 results from Bag of words algorithm are:")
+    for qa_pair,score in sortedResults:
+        if count < RESULTS_TOPN:
+            print(qa_pair.answer,score)
+            count = count + 1
+
+    #TODO: Now add the NLP engine algorithm
 
 if __name__ == "__main__":
     main()
--- nlp_algo.py
+++ nlp_algo.py
@@ -1,13 +1,16 @@
-class NLPAlgorithm:
-    def __init__(self, uquestion):
-        self.user_question = uquestion
-        
-        #This will contain the evaluation score
-        self.score = None
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from collections import defaultdict
 
+class NLPAlgorithm:
+    def __init__(self, uquestion, qfeat, docfeat):
+        self.user_question = uquestion
+        #This will contain the qapair:score
+        self.scoreDict = defaultdict()
+        self.qafeat = qfeat
+        self.docs_feature = docfeat
         #Final results
-        self.results = None
-        
+        self.results = list()
     '''Private abstract function to implement the actual algorithm'''
     def _compute(self):
         raise NotImplementedError("Class %s doesn't implement _compute()" % (self.__class__.__name__))
@@ -21,11 +24,36 @@ class NLPAlgorithm:
         print("Final Score is " + self.score)
 
 class BOWAlgorithm(NLPAlgorithm):
-    def __init__(self, uquestion):
-        super().__init__(uquestion)
+    def __init__(self, uquestion, qfeat, docfeat):
+        super().__init__(uquestion, qfeat, docfeat)
 
+    def __compute_cosine(self, query, doc):
+        query = np.array(query).reshape(1,-1)
+        doc = np.array(doc).reshape(1,-1)
+        return cosine_similarity(query, doc)
     def _compute(self):
-        pass
-    
+
+        '''
+        TODO: QAFeatureExtraxction object has qa_pairs and _bow in the same order.
+        This works as both are sequentially accessed. So _bow index can be used to
+        access corresponding qa_pair
+        '''
+        query_vec = list(self.qafeat.bow[0].values())
+        for index, faq_bow in enumerate(self.docs_feature.bow):
+            faq_vec = []
+            for word in self.qafeat.bow[0]:
+                if word in faq_bow:
+                    faq_vec.append(faq_bow[word])
+                else:
+                    faq_vec.append(0)
+            if len(faq_vec) != 0:
+                #cosine similarity returns in numpy array. Convert it into regular val
+                simScore = self.__compute_cosine(query_vec, faq_vec).tolist()[0][0]
+                self.scoreDict[self.docs_feature.qa_pairs[index]] = simScore
+            else:
+                print("No matching words found")
+        return self.scoreDict
     def _evaluate(self):
-        pass
+       #Use the Evaluation objects here
+ 
+       pass
