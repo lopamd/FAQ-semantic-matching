ebc21d3636f1d9c878a923b72ec3ac0f9f8afb10
Lopamudra Muduli <lopa147@gmail.com>
2018-04-27 18:05:28 -0700

    Added evaluation.csv to config file.
    Signed-off-by: Lopamudra Muduli <lopa147@gmail.com>

--- nlp_config.py
+++ nlp_config.py
@@ -3,6 +3,7 @@ import os
 default_locale = 'english'
 
 faq_input_file = 'input/Hummingbirds.csv'
+evaluation_input_file = 'input/evaluationInput.csv'
 
 path_to_stanford_lib = r'deps/stanford-corenlp-full-2018-02-27'
 path_to_stanford_jar = path_to_stanford_lib + r'/stanford-corenlp-3.9.1.jar'
